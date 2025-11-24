eaf2ad7c14f9ead13c83a39fd7328253f9f4223d
Ludovico Bellio <ludovico.bellio@itiszuccante.edu.it>
2025-11-24 11:27:46 +0100

    scheletro di gamepage

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -7,11 +7,61 @@ class GamePage extends StatefulWidget {
   State<GamePage> createState() => _GamePageState();
 }
 
+class MyButton {
+  Widget newButton() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: FloatingActionButton(
+        onPressed: () {
+          print("pressed");
+        },
+      ),
+    );
+  }
+}
+
 class _GamePageState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [Row(children: [])]),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyButton().newButton(),
+                MyButton().newButton(),
+                MyButton().newButton(),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyButton().newButton(),
+                MyButton().newButton(),
+                MyButton().newButton(),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyButton().newButton(),
+                MyButton().newButton(),
+                MyButton().newButton(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
